32c1d5c4f1b9e1dd95ceb670882af23fded9d431
khadeejamp000 <shamseercpcalicut@gmail.com>
2021-08-26 12:28:59 +0530

    fourth commit

--- second.js
+++ second.js
@@ -54,3 +54,107 @@ for (var i = 0; i < arr1.length; i++) {
 document.write(item + "is most frequent item")
 
 alert(item + " ( " + mf + " times ) ");
+document.write("<br>");
+
+document.write("<br>");
+
+var str = prompt("enter the string");
+var UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var LOWER = 'abcdefghijklmnopqrstuvwxyz';
+var result = [];
+  
+  for(var x=0; x<str.length; x++)
+  {
+    if(UPPER.indexOf(str[x]) !== -1)
+    {
+      result.push(str[x].toLowerCase());
+    }
+    else if(LOWER.indexOf(str[x]) !== -1)
+    {
+      result.push(str[x].toUpperCase());
+    }
+    else 
+    {
+      result.push(str[x]);
+    }
+  }
+document.write(result.join(''));
+document.write("<br>");
+
+const num11 = parseInt(prompt('Enter the number ')); 
+  for( var i=0;1<16;i++)
+  {
+    if(num11%2===0)
+    {
+      document.write("even")
+    }
+    else
+    {
+      document.write("odd")
+
+    }
+    break;
+  }
+  document.write("<br>");
+
+  var length = 3;
+var myString = prompt("enter your string");
+var myTruncatedString = myString.substring(0,length);
+document.write(myTruncatedString)
+
+document.write("<br>");
+
+const numb6 = parseInt(prompt('Enter the number ')); 
+const numb7 = parseInt(prompt('Enter the number ')); 
+if(numb6>numb7){
+  document.write(numb6+" is largest");
+}
+else{
+  document.write(numb7+" is largest");
+
+}
+document.write("<br>");
+document.write("<br>");
+
+
+
+const string = prompt('Enter a array: ');
+
+const words = string.split(' ');
+
+  const z=words.sort();
+
+document.write( "the sorted words are :"+ z)
+// console.log('The sorted words are:');
+
+for (const element of words) {
+  console.log(element);
+}
+document.write("<br>");
+document.write("<br>");
+var str="javascript";
+for( var i=0;i<=9;i++)
+{
+    for(var j=0;j<i;j++)
+    {
+        document.write( "&nbsp &nbsp"+str[j]);
+    }
+    document.write("&nbsp &nbsp\n");
+    document.write("<br>");
+
+    }
+    document.write("<br>");
+    document.write("<br>");
+
+document.write("<br>");
+var input=parseInt(prompt)
+function sum_sq(array) {
+  var sum = 0, 
+      i = array.length;
+  while (i--) 
+   sum += Math.pow(array[i], 2);
+  return sum;
+}
+ 
+ document.write(sum_sq([0,1,2,3,4])); 
+
